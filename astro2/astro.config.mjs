c653b2cb9d97c2fed9bdbfe0fc330447bbf1b536
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-11-25 18:22:16 +0800

    feat: astro starlight-blog poc

---
+++ astro2/astro.config.mjs
@@ -0,0 +1,29 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			plugins: [starlightBlog()],
+			title: 'Tsung-Han Yu',
+			favicon: 'favicon.ico',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/johan456789/johan456789.github.io' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
