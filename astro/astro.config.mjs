ce69a1b37d79c398ad98900fc4ff23f4d1784bc6
Tsung-Han Yu <14802181+johan456789@users.noreply.github.com>
2025-11-25 19:04:54 +0800

    refactor: change folder name

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,12 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+			plugins: [starlightBlog()],
 			title: 'Tsung-Han Yu',
+			favicon: 'favicon.ico',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/johan456789/johan456789.github.io' }],
 			sidebar: [
 				{
@@ -14,15 +17,6 @@ export default defineConfig({
 					items: [
 						// Each item here is one entry in the navigation menu.
 						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Blog',
-					items: [
-						{ label: 'First Post - Why Blog', slug: 'posts/first-post' },
-						{ label: 'Why Password Managers Are Both More Convenient and Secure', slug: 'posts/why-password-managers-are-both-more-convenient-and-secure' },
-						{ label: 'Blog Update', slug: 'posts/blog-update' },
-						{ label: 'Book Review: Collywobbles', slug: 'posts/book-review-collywobbles' },
 					],
 				},
 				{
